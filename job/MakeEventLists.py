04f2cf4df8d1103eec431afa09f1bc91ecfe9369
Krishan Mistry <kvjmistry@gmail.com>
2025-05-07 10:51:02 -0500

    Save pressure and diffusion notebooks

--- job/MakeEventLists.py
+++ job/MakeEventLists.py
@@ -17,19 +17,17 @@ def MakeList(mode, pressure, diff):
     print(f"File list saved to {output_file}")
 
 
-modes = ["ATPC_0nubb", "ATPC_Bi", "ATPC_Tl"]
-pressures = ["1bar", "5bar", "10bar", "15bar"]
-diffs = ["0.05percent", "0.1percent", "0.25percent", "0.5percent", "5percent", "nodiff"]
+modes = ["ATPC_0nubb", "ATPC_Bi", "ATPC_Tl", "ATPC_single"]
+pressures = ["1bar", "5bar", "10bar", "15bar", "25bar"]
+diffs = ["0.1percent", "0.25percent", "0.0percent", "5percent", "nodiff"]
 
 for m in modes:
     for p in pressures:
-        
+
         if (p != "1bar"):
             diffs = ["5percent", "nodiff"]
         else:
-            diffs = ["0.1percent", "0.25percent", "5percent", "nodiff"]
-        
+            diffs = ["0.1percent", "0.25percent", "5percent", "nodiff", "0.0percent"]
+
         for d in diffs:
             MakeList(m, p, d)
-
-
