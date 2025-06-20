387e7fc73a336a6bd3702c61156ec316c40e802c
Krishan Mistry <kvjmistry@gmail.com>
2025-06-20 15:40:55 -0500

    Add thickness generator job scripts

---
+++ job/RunGammaGeneration.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH -J GammaGeneration # A single job name for the array
+#SBATCH --nodes=1
+#SBATCH --mem 4000 # Memory request (6Gb)
+#SBATCH -t 0-6:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o GammaGeneration_%A_%a.out # Standard output
+#SBATCH -e GammaGeneration_%A_%a.err # Standard error
+
+start=`date +%s`
+
+# Setup nexus and run
+echo "Setting up environment"
+source /home/argon/Projects/Krishan/venv/bin/activate
+source /home/argon/Projects/Krishan/nexus/setup_cluster.sh
+
+Enrichment=$1
+Pressure=$2
+Thickness=$3
+MODE=$4
+CubeSize=$5
+
+echo "Enrichment mode is: ${Enrichment}"
+echo "Pressure is: ${Pressure}"
+echo "Thickness is: ${Thickness}"
+echo "Gamma mode is: ${MODE}"
+echo "Cube Size is: ${CubeSize}"
+
+CONFIG=ATPC_${MODE}.config.mac
+INIT=ATPC_${MODE}.init.mac
+
+mkdir -p /media/argon/HardDrive_8TB/Krishan/ATPC/GammaThickness/${MODE}/${Thickness}cm/${Pressure}bar/${Enrichment}/
+cd       /media/argon/HardDrive_8TB/Krishan/ATPC/GammaThickness/${MODE}/${Thickness}cm/${Pressure}bar/${Enrichment}/
+
+cp /home/argon/Projects/Krishan/ATPC/config/${CONFIG} .
+cp /home/argon/Projects/Krishan/ATPC/config/${INIT} .
+cp /home/argon/Projects/Krishan/ATPC/scripts/SmearEnergy.py .
+cp /home/argon/Projects/Krishan/ATPC/scripts/CompressEvents.py .
+
+# Copy the ion generator disrupter file
+if [ "$MODE" = "Bi_ion" ]; then
+    cp /home/argon/Projects/Krishan/ATPC/config/Bi214.mac .
+fi
+
+SEED=$((${SLURM_ARRAY_TASK_ID} + 1))
+sed -i "s#.*random_seed.*#/nexus/random_seed ${SEED}#" ${CONFIG}
+sed -i "s#.*gas_pressure.*#/Geometry/ATPC/gas_pressure ${Pressure} bar#" ${CONFIG}
+sed -i "s#.*chamber_thickn.*#/Geometry/ATPC/chamber_thickn ${Thickness} cm#" ${CONFIG}
+sed -i "s#.*output_file.*#/nexus/persistency/output_file ATPC_${MODE}_${Enrichment}_${Pressure}bar#" ${CONFIG}
+sed -i "s#.*cube_size.*#/Geometry/ATPC/cube_size ${CubeSize} m#" ${CONFIG}
+
+cat ${CONFIG}
+cat ${INIT}
+
+nexus ${INIT} -n 10000000
+
+
+python3 CompressEvents.py ATPC_${MODE}_${Enrichment}_${Pressure}bar
+python3 SmearEnergy.py ATPC_${MODE}_${Enrichment}_${Pressure}bar
+
+echo "FINISHED....EXITING"
+
+end=`date +%s`
+let deltatime=end-start
+let hours=deltatime/3600
+let minutes=(deltatime/60)%60
+let seconds=deltatime%60
---
+++ job/SubGammaThickness.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+MODE=Bi_ion
+
+# 1 bar enriched
+Enrichment=enr; Pressure=1; Thickness=4; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=1; Thickness=6; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=1; Thickness=8; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=1; Thickness=10; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=1; Thickness=12; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=1; Thickness=18; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+
+# 5 bar enriched
+Enrichment=enr; Pressure=5; Thickness=4; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=5; Thickness=6; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=5; Thickness=8; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=5; Thickness=10; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=5; Thickness=12; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=5; Thickness=18; CubeSize=3.615
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 10 bar enriched
+Enrichment=enr; Pressure=10; Thickness=4; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=6; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=8; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=10; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=12; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=18; CubeSize=2.870
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 15 bar enriched
+Enrichment=enr; Pressure=15; Thickness=4; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=15; Thickness=6; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=15; Thickness=8; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=15; Thickness=10; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=15; Thickness=12; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=15; Thickness=18; CubeSize=2.507
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 25 bar enriched
+Enrichment=enr; Pressure=25; Thickness=4; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=25; Thickness=6; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=25; Thickness=8; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=25; Thickness=10; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=25; Thickness=12; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=25; Thickness=18; CubeSize=2.114
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# =====================================================================================
+
+
+# 1 bar natural
+Enrichment=nat; Pressure=1; Thickness=4; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=1; Thickness=6; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=1; Thickness=8; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=1; Thickness=10; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=1; Thickness=12; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=1; Thickness=18; CubeSize=13.319
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+
+# 5 bar natural
+Enrichment=nat; Pressure=5; Thickness=4; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=5; Thickness=6; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=5; Thickness=8; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=5; Thickness=10; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=5; Thickness=12; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=5; Thickness=18; CubeSize=7.789
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 10 bar natural
+Enrichment=nat; Pressure=10; Thickness=4; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=10; Thickness=6; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=10; Thickness=8; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=10; Thickness=10; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=10; Thickness=12; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=enr; Pressure=10; Thickness=18; CubeSize=6.182
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 15 bar natural
+Enrichment=nat; Pressure=15; Thickness=4; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=15; Thickness=6; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=15; Thickness=8; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=15; Thickness=10; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=15; Thickness=12; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=15; Thickness=18; CubeSize=5.301
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+# 25 bar natural
+Enrichment=nat; Pressure=25; Thickness=4; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=25; Thickness=6; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=25; Thickness=8; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=25; Thickness=10; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=25; Thickness=12; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
+
+Enrichment=nat; Pressure=25; Thickness=18; CubeSize=4.556
+sbatch --array=1-10 RunGammaGeneration.sh $Enrichment $Pressure $Thickness $MODE $CubeSize
